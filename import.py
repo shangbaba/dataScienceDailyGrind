870b430e0756da6bc09ac527cead9a097a34c78a
Elvin Gao <elvingaocn@gmail.com>
2018-11-16 20:57:45 +1100

    Import libraries

---
+++ import.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import scipy
+
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+import seaborn as sb
+
+import sklearn
+from sklearn import preprocessing
+from sklearn.preprocessing import scale
+
+%matplotlib inline
+rcParams['figure.figsize'] = 5, 4
+sb.set_style ('darkgrid')
